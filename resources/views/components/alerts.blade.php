f61e4d948fe89764c063e768242575a071ef9716
CarolineVantim <carolvantim@gmail.com>
2023-12-25 21:49:09 -0300

    🚀feature: styles

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -1,14 +1,14 @@
 @if ($errors->any())
     @foreach ($errors->all() as $error)
-        <div class="alert-error">
-            <p>{{ $error }}</p>
+        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+            <span class="font-medium">{{ $error }}</span>
         </div>
     @endforeach
 @endif
 
 
 @if (session()->has('message'))
-    <div class="alert-sucess">
-        {{ session('message')}}
+    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400">
+        <span class="font-medium">{{ session('message') }}</span>
     </div>
 @endif
